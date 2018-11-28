f1fc42e8c7670e03ed827618778321c6e6b5fe53
Istvan Szalontai <istvan.szalontai12@gmail.com>
2018-11-28 11:21:14 +0100

    Game and masterclass

---
+++ CanvasGame/src/com/szalontaiworks/core/Main.java
@@ -0,0 +1,10 @@
+package com.szalontaiworks.core;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hreeew");
+	}
+
+}
--- Java Prog Masterclass/src/com/szalontaiworks/Variables.java
+++ Java Prog Masterclass/src/com/szalontaiworks/Variables.java
@@ -3,7 +3,9 @@ package com.szalontaiworks;
 public class Variables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int myInt = 5;
+		float myFloat = 5.4f;
+		double myDouble = 5.4d;
 		System.out.println("Hello");
 	}
 
